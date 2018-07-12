703042056a83fa62c4818e741cd5a3c639d6cac8
Vladimir Shmalko <vladimir@thinair.coim>
2018-07-12 16:33:49 -0700

    Initial comit

---
+++ src/pretxt.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import argparse
+
+from jinja2 import Environment
+
+from yaml import load
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+parser = argparse.ArgumentParser(description='Pretxt, render a template with provided values')
+parser.add_argument('-c', '--config', help='config file (YAML)', type=str)
+parser.add_argument("input", help="input file (Jinja2 template)", type=str)
+args = parser.parse_args()
+print(args)
+
+def read_file(fname):
+  res = None
+  if os.path.isfile(fname):
+    try:
+      with open(fname, 'r') as f:
+        res = f.read()
+    except Exception as e:
+      print('File read error {}: {}'.format(fname, e))
+      sys.exit(2)
+  else:
+    print('File not found: {}'.format(fname))
+    sys.exit(2)
+
+  return  res
+
+def main():
+  input_f = read_file(args.input)
+  config_f = read_file(args.config)
+
+  # Read YAML
+  config = load(config_f, Loader=Loader)
+  print(config)
+
+  env = Environment()
+  templ = env.from_string(source=input_f)
+  # print(env)
+  # print(templ)
+  print(templ.render(config))
+
+if __name__ == '__main__':
+  main()
